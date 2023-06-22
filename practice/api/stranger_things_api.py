f9999210da84c621c24b7e3b5ba6d9c624c450ed
Stefan Iancu <iancustefan28@yahoo.com>
2023-06-22 17:30:56 +0300

    scheduler

--- practice/api/stranger_things_api.py
+++ practice/api/stranger_things_api.py
@@ -1,3 +1,4 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
 import requests
 import time
 
@@ -18,12 +19,16 @@ def get_stranger_quote(url):
             author = i["author"]
             print(f"{quote} - {author}")
 
+# sched = BlockingScheduler()
 
-while True:
+# @sched.scheduled_job('interval', seconds=10)
+# def timed_job():
+#     print('This job is run every 10 seconds.')
 
-    try:
-        time.sleep(3)    
-        get_stranger_quote(URL)
-    except requests.ConnectTimeout:
-        print("Connection error!")
+# # @sched.scheduled_job('cron', day_of_week='mon-fri', hour=10)
+# # def scheduled_job():
+# #     print('This job is run every weekday at 10am.')
+
+# # sched.configure(options_from_ini_file)
+# sched.start()
 
